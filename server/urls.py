d4fb1b15478823a11d81095b1f409bc12cae4b5e
Alexandre Blanchet <alexandreblanchet08@gmail.com>
2020-03-05 16:01:03 +0100

    adding tictactoe project

--- server/urls.py
+++ server/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('serverChat/', include('serverChat.urls'))
+    path('serverChat/', include('serverChat.urls')),
+    path('login/', include('authenticate.urls'))
 ]
